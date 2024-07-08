b611e0bb242a6dea3478cf6ec34db0927a6eccc6
Mariel Anahi Risso <marielanahirisso@gmail.com>
2024-07-08 13:03:08 -0300

    Primera versión del proyecto Alura Cinema

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,14 @@
 const { default: Inicio } = require("pages/Inicio");
 const { BrowserRouter, Routes } = require("react-router-dom");
 
-function AppRoutes(){
-return(
-<BrowserRouter>
-<Routes>
-<Route path="/" element={<Inicio/>}></Route>
-</Routes>
-
-</BrowserRouter>
-)
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default AppRoutes;
